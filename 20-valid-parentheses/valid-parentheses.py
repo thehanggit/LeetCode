847756f23ca20e002ad20b8724aa37b206f2c5d8
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2024-12-31 13:02:22 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (10.94%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        matching = {"(":")", "[":"]", "{":"}"}
+
+        for ch in s:
+            if ch in matching:
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                
+                last_paretheses = stack.pop()
+                if matching[last_paretheses] != ch:
+                    return False
+        
+        return not stack
+                    
