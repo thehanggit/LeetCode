115abb891294f330901ee49ea9d3045fa4fdd0e1
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-03 23:34:13 -0800

    Time: 3 ms (82.34%) | Memory: 17.8 MB (11.88%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,38 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        queue = deque()
+        time, freshcount = 0, 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    freshcount += 1
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+
+        if freshcount == 0:
+            return 0
+        if not queue:
+            return -1
+
+        dirs = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+
+        while queue:
+            size = len(queue)
+            while size > 0:
+                size -= 1
+                i, j = queue.popleft()
+                for dir in dirs:
+                    new_x = i + dir[0]
+                    new_y = j + dir[1]
+                    if 0 <= new_x < rows and 0 <= new_y < cols and grid[new_x][new_y] == 1:
+                        queue.append((new_x, new_y))
+                        grid[new_x][new_y] = 2
+                        freshcount -= 1
+            time += 1
+        if freshcount == 0:
+            return time - 1
+        else:
+            return -1
