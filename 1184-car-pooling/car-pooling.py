238f0893eb44909fcb7a4d9ef22b5063942f0bda
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-04 16:27:08 -0800

    Time: 1 ms (77.91%) | Memory: 18.3 MB (21.61%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,11 +1,16 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        path = [0] * 1000
-        for trip in trips:
-            num, start, end = trip
-            for i in range(start, end):
-                path[i] += num
-                if path[i] > capacity:
-                    return False
-        
+        arr = []
+        for i in range(len(trips)):
+            arr.append((trips[i][1],1,trips[i][0]))
+            arr.append((trips[i][2],-1,trips[i][0]))
+        arr.sort()
+        cap = 0
+        for a in arr:
+            if a[1] == 1:
+                cap += a[2]
+            else:
+                cap -= a[2]
+            if cap > capacity:
+                return False
         return True
