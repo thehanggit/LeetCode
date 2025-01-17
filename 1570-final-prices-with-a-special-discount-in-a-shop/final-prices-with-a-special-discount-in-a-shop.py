aa86e18fbeae23d08e306bc564d54917e0a37a5f
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-16 22:11:00 -0800

    Time: 3 ms (53.66%) | Memory: 17.8 MB (45.57%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        ans = prices.copy()
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[i] >= prices[j]:
+                    ans[i] = prices[i] - prices[j]
+                    break
+        return ans
