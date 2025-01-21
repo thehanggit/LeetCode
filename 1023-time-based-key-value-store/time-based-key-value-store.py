903eacd0471b39473be9ca6109ef01af037f31c9
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-20 23:47:32 -0800

    Time: 149 ms (27.87%) | Memory: 75.4 MB (21.35%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,27 +1,48 @@
 class TimeMap:
-
     def __init__(self):
         self.dict = defaultdict(list)
+
+    # def set(self, key: str, value: str, timestamp: int) -> None:
+    #     if key not in self.dict:
+    #         self.dict[key] = []
+    #     self.dict[key].append([value,  timestamp])
+        
+
+    # def get(self, key: str, timestamp: int) -> str:
+    #     res = ""
+    #     if key not in self.dict:
+    #         return res
+    #     values = self.dict.get(key,[])
+    #     l, r = 0, len(values) - 1
+    #     while l <= r:
+    #         mid = (l + r)//2
+    #         if values[mid][1] <= timestamp:
+    #             l = mid + 1
+    #             res = values[mid][0]
+    #         else:
+    #             r = mid - 1
+    #     return res
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.dict:
             self.dict[key] = []
-        self.dict[key].append([value,  timestamp])
-        
+        self.dict[key].append([value, timestamp])
+
 
     def get(self, key: str, timestamp: int) -> str:
         res = ""
         if key not in self.dict:
             return res
         values = self.dict.get(key,[])
-        l, r = 0, len(values) - 1
-        while l <= r:
-            mid = (l + r)//2
+        left = 0
+        right = len(values) - 1
+        while left <= right:
+            mid = (left + right) // 2
             if values[mid][1] <= timestamp:
-                l = mid + 1
+                left = mid + 1
                 res = values[mid][0]
             else:
-                r = mid - 1
+                right = mid - 1
         return res
 
 
