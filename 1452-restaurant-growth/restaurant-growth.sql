7384e2c94166c758c50325c7db2c69f20c99dec2
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-25 23:04:13 -0800

    Time: 779 ms (19.44%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+with finance as (
+    select
+        distinct visited_on,
+        sum(amount) over (order by visited_on range between interval 6 day preceding and current row) amount,
+        min(visited_on) over() 1st_date
+    from
+        Customer
+)
+
+select
+    visited_on,
+    amount,
+    round(amount/7, 2) as average_amount
+from
+    finance
+where
+    visited_on >= 1st_date+6
