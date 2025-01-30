97d7e2012d549753fc77944ed352d9b5b1edbf3e
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-30 01:16:41 -0800

    Time: 3934 ms (5.11%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+select
+    E.left_operand,
+    E.operator,
+    E.right_operand,
+    case
+        when E.operator = '<' and V1.value < V2.value then 'true'
+        when E.operator = '=' and V1.value = V2.value then 'true'
+        when E.operator = '>' and V1.value > V2.value then 'true'
+        else 'false'
+    end as value
+
+from
+    Expressions E
+join
+    Variables V1
+on
+    E.left_operand = V1.name
+join
+    Variables V2
+on
+    E.right_operand = V2.name
