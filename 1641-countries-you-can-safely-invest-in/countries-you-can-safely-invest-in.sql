3f5b78239b79968137f82e45b5ff09dab2050e34
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-31 00:53:04 -0800

    Time: 2228 ms (28.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+with person_country_code as (
+    select
+        *,
+        substring(phone_number, 1, 3) as  country_code
+    from
+        Person
+)
+
+select
+    C.name as country
+from
+    Country C
+left join
+    person_country_code as P
+on
+    C.country_code = P.country_code
+left join
+    Calls CS
+on
+    P.id in (CS.caller_id, CS.callee_id)
+group by
+    C.country_code
+having
+    avg(duration) > (select avg(duration) from Calls)
