21503da79fb9fbb775c3836d11bacba643124e78
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-01-30 19:30:51 -0800

    Time: 541 ms (92.87%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,25 +1,35 @@
 # Write your MySQL query statement below
-with distinct_pair as (
-    select
-        case
-            when from_id < to_id then from_id
-            when from_id > to_id then to_id
-        end as person1,
-        case
-            when from_id < to_id then to_id
-            when from_id > to_id then from_id
-        end as person2,
-        duration
-    from
-        Calls
-)
+-- with distinct_pair as (
+--     select
+--         case
+--             when from_id < to_id then from_id
+--             when from_id > to_id then to_id
+--         end as person1,
+--         case
+--             when from_id < to_id then to_id
+--             when from_id > to_id then from_id
+--         end as person2,
+--         duration
+--     from
+--         Calls
+-- )
+
+-- select
+--     person1,
+--     person2,
+--     count(*) as call_count,
+--     sum(duration) as total_duration
+-- from
+--     distinct_pair
+-- group by
+--     person1, person2
 
 select
-    person1,
-    person2,
+    least(from_id, to_id) as person1,
+    greatest(from_id, to_id) as person2,
     count(*) as call_count,
     sum(duration) as total_duration
 from
-    distinct_pair
+    Calls
 group by
     person1, person2
