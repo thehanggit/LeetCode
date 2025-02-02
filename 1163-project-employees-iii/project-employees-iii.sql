47557ce19c7646a3a36f0163e409a517c86286c7
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-02-01 16:03:36 -0800

    Time: 1482 ms (12.49%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,29 +1,48 @@
 # Write your MySQL query statement below
-with project_info as (
-    select
-        P.project_id,
-        E.*
-    from
-        Employee E
-    left join
-        Project P
-    on
-        P.employee_id = E.employee_id
-)
+-- with project_info as (
+--     select
+--         P.project_id,
+--         E.*
+--     from
+--         Employee E
+--     left join
+--         Project P
+--     on
+--         P.employee_id = E.employee_id
+-- )
+
+-- select
+--     project_id,
+--     employee_id
+-- from
+--     project_info
+-- where
+--     (project_id, experience_years) in (
+--         select
+--             project_id,
+--             max(experience_years) as most_experience
+--         from
+--             project_info
+--         group by
+--             project_id
+        
+--     )
 
 select
-    project_id,
-    employee_id
+    t.project_id,
+    t.employee_id
 from
-    project_info
-where
-    (project_id, experience_years) in (
+    (
         select
             project_id,
-            max(experience_years) as most_experience
+            p.employee_id,
+            rank() over(partition by project_id order by experience_years desc) as rnk
         from
-            project_info
-        group by
-            project_id
-        
-    )
+            Project p
+        join
+            Employee e
+        on
+            p.employee_id = e.employee_id
+    ) t
+where
+    t.rnk = 1
