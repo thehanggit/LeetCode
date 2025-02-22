474cd24bf0692caddc89e56b3745a7e354dbe6ef
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-02-22 11:07:27 -0800

    Time: 800 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,43 +1,13 @@
-with start_cte as (
-    select
-        machine_id,
-        process_id,
-        timestamp
-    from
-        Activity
-    where
-        activity_type = 'start'
-),
-
-end_cte as (
-    select
-        machine_id,
-        process_id,
-        timestamp
-    from
-        Activity
-    where
-        activity_type = 'end'
-),
-
-cte as (
-    select
-        s.machine_id,
-        s.process_id,
-        e.timestamp - s.timestamp as time
-    from
-        start_cte s
-    join
-        end_cte e
-    on
-        s.machine_id = e.machine_id
-        and s.process_id = e.process_id
-)
-
 select
-    machine_id,
-    round(avg(time), 3) as processing_time
+    a.machine_id,
+    round(avg(b.timestamp - a.timestamp) , 3) as processing_time
 from
-    cte
+    Activity a,
+    Activity b
+where
+    a.machine_id = b.machine_id
+    and a.process_id = b.process_id
+    and a.activity_type = 'start'
+    and b.activity_type = 'end'
 group by
     machine_id
