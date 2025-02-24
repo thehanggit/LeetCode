b77308bc08154131706badcbcf116bffe1d6b173
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-02-23 23:59:24 -0800

    Time: 115 ms (70.43%) | Memory: 55.4 MB (16.80%) - LeetSync

---
+++ 833-bus-routes/bus-routes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:
+            return 0
+        
+        graph = collections.defaultdict(set)
+        queue = collections.deque([(source, 0)])
+
+        for bus, route in enumerate(routes):
+            for stop in route:
+                graph[stop].add(bus)
+
+        visited_stops = set()
+        visited_buses = set()
+
+        while queue:
+            stop, route_len = queue.popleft()
+
+            if stop == target:
+                return route_len
+            
+            for bus in graph[stop]:
+                if bus not in visited_buses:
+                    visited_buses.add(bus)
+
+                    for stop in routes[bus]:
+                        if stop not in visited_stops:
+                            visited_stops.add(stop)
+                            queue.append((stop, route_len + 1))
+        return -1 
+
