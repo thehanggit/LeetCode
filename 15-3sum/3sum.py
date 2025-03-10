1fac0b05f6d7a59888f79464bdcc7b68fb30e918
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-03-10 08:18:44 -0700

    Time: 469 ms (83.89%) | Memory: 20.7 MB (39.35%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,18 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # ans = []
+        # nums.sort()
+        # for i in range(len(nums)):
+        #     if nums[i] > 0:
+        #         break
+        #     if i > 0 and nums[i - 1] == nums[i]:
+        #         continue
+            
+        #     j = i + 1
+        #     k = len(nums) - 1
+        #     while j < k:
+        #         total = nums[i] + nums[j] + nums[k]
+                
+        #         if total == 0:
+        #             ans.append([nums[i], nums[j], nums[k]])
+        #             j += 1
+        #             k -= 1
+        #             while j < k and nums[j] == nums[j - 1]:
+        #                 j += 1
+        #             while j < k and nums[k] == nums[k + 1]:
+        #                 k -= 1
+                
+        #         elif total > 0:
+        #             k -= 1
+        #         else:
+        #             j += 1
+            
+        # return ans
         ans = []
         nums.sort()
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            if i > 0 and nums[i - 1] == nums[i]:
+            if i > 0  and nums[i - 1] == nums[i]:
                 continue
-            
             j = i + 1
             k = len(nums) - 1
             while j < k:
                 total = nums[i] + nums[j] + nums[k]
-                
+
                 if total == 0:
                     ans.append([nums[i], nums[j], nums[k]])
                     j += 1
@@ -21,10 +48,9 @@ class Solution:
                         j += 1
                     while j < k and nums[k] == nums[k + 1]:
                         k -= 1
-                
                 elif total > 0:
                     k -= 1
                 else:
                     j += 1
-            
         return ans
+            
