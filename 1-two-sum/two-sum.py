0fc61fc5d7b1af4945b57fd0a67008a153e9d681
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-03-10 07:39:14 -0700

    Time: 2 ms (54.34%) | Memory: 19.1 MB (24.56%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,38 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            num = nums[i]
-            remain = target - nums[i]
-            if remain in hashmap:
-                return [i, hashmap[remain]]
-            
-            hashmap[num] = i
-        return [-1, -1] 
+        # hashmap = {}
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in hashmap:
+        #         return [i, hashmap[complement]]
+        #     else:
+        #         hashmap[nums[i]] = i
+        # return [-1, -1]
                 
+    
+        # hashmap = {}
+        # ans = []
+        # for i, num in enumerate(nums):
+        #     hashmap[num].append(i)
+            
+        # for num, indicies in hashmap.items():
+        #     complement = target - num
+        #     if complement in hashmap:
+        #         if complement == num:
+        #             for i in range(len(indicies)):
+        #                 for j in range(i+1, len(indicies)):
+        #                     ans.append([indicies[i], indicies[j]])
+                
+        #         elif complement < num:
+        #             for i in indicies:
+        #                 for j in hashmap[complement]:
+        #                     ans.append([i, j])
+
+        hashmap = defaultdict(int)
+        for i in range(len(nums)):
+            residual = target - nums[i]
+            if residual in hashmap:
+                return [i, hashmap[residual]]
+            else:
+                hashmap[nums[i]] = i
+        return [-1, -1]
