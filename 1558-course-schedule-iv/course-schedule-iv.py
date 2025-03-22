7a52cdba9e276701472450e02ad5d890d20a86d4
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-03-21 18:08:58 -0700

    Time: 706 ms (13.39%) | Memory: 20.2 MB (97.41%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,9 +1,11 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         adj = [[] for _ in range(numCourses)]
+
         for pre in prerequisites:
             adj[pre[1]].append(pre[0])
-        
+
+
         def dfs(u, v, visited):
             if u in adj[v]:
                 return True
