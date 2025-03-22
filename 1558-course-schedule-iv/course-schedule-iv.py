5a232abf3d6c7c375ae8912fc37bfea8e7e346e2
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-03-21 18:32:48 -0700

    Time: 74 ms (54.72%) | Memory: 20.4 MB (55.39%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,22 +1,52 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        # adj = [[] for _ in range(numCourses)]
+
+        # for pre in prerequisites:
+        #     adj[pre[1]].append(pre[0])
+
+
+        # def dfs(u, v, visited):
+        #     if u in adj[v]:
+        #         return True
+        #     for neighbor in adj[v]:
+        #         if neighbor not in visited:
+        #             visited.add(neighbor)
+        #             if dfs(u, neighbor, visited):
+        #                 return True
+        #     return False
+        
+        # ans = []
+        # for u, v in queries:
+        #     ans.append(dfs(u, v, set()))
+        # return ans
+
         adj = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
 
         for pre in prerequisites:
-            adj[pre[1]].append(pre[0])
+            adj[pre[0]].append(pre[1])
+            indegree[pre[1]] += 1
+
+        nodesprerequisites = defaultdict(set)
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        while q:
+            course = q.popleft()
+            for neighbor in adj[course]:
+                nodesprerequisites[neighbor].add(course)
+                for prereq in nodesprerequisites[course]:
+                    nodesprerequisites[neighbor].add(prereq)
+                
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+
+        return [u in nodesprerequisites[v] for u, v in queries]
 
 
-        def dfs(u, v, visited):
-            if u in adj[v]:
-                return True
-            for neighbor in adj[v]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    if dfs(u, neighbor, visited):
-                        return True
-            return False
-        
-        ans = []
-        for u, v in queries:
-            ans.append(dfs(u, v, set()))
-        return ans
+
+
