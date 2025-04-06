567ac4d07a26a250da101ee560375357c97a801b
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-05 17:49:50 -0700

    Time: 9 ms (42.74%) | Memory: 21 MB (63.26%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -16,16 +16,45 @@ class Solution:
         #     ans.append(heapq.heappop(heap))
         
         # return ans
-        intervals.sort(key=lambda x:x[0])
-        merged = [intervals[0]]
-        for i in range(1, len(intervals)):
-            prev = merged[-1]
-            if prev[1] >= intervals[i][0]:
-                prev[1] = max(prev[1], intervals[i][1])
-            else:
-                merged.append(intervals[i])
+        # intervals.sort(key=lambda x:x[0])
+        # merged = [intervals[0]]
+        # for i in range(1, len(intervals)):
+        #     prev = merged[-1]
+        #     if prev[1] >= intervals[i][0]:
+        #         prev[1] = max(prev[1], intervals[i][1])
+        #     else:
+        #         merged.append(intervals[i])
 
-        return merged
+        # return merged
+        
+        # sort based on the start time
+        # intervals.sort(key=lambda x: x[0])
+        # merged = [intervals[0]]
+        # for i in range(1, len(intervals)):
+        #     prev = merged[-1]
+        #     if prev[1] >= intervals[i][0]:
+        #         prev[1] = max(prev[1], intervals[i][1])
+        #     else:
+        #         merged.append(intervals[i])
+        
+        # return merged
+
+
+        # first sort the intervals based on start_date
+        # second find out if the end date conflicts with the next interval's start date, if so, merge, otherwise, append it into result
+        intervals.sort(key=lambda x:x[0])
+        merge = [intervals[0]]
+        for i in range(1, len(intervals)):
+            prev_end = merge[-1][1]
+            next_start = intervals[i][0]
+            if prev_end >= next_start:
+                merge[-1][1] = max(intervals[i][1], prev_end)
+            else:
+                merge.append(intervals[i])
+        return merge
+
+
+
 
 
 
