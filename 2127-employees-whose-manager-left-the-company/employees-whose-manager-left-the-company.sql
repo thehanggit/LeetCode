0ee9f320459eba1bb7b0f9824911324e41402a78
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-09 09:58:12 -0700

    Time: 315 ms (97.92%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -4,12 +4,7 @@ select
 from
     Employees
 where
-    salary < 30000
-    and manager_id not in (
-        select
-            employee_id
-        from
-            Employees
-    )
+    manager_id not in (select employee_id from Employees)
+    and salary < 30000
 order by
     employee_id
