0c1552bc9eb7c8495f7d8198c9c9d4c58d6442a4
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-09 14:53:50 -0700

    Time: 534 ms (93.88%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,26 +1,20 @@
 with first_login as (
     select
         player_id,
-        min(event_date) as first_date
+        min(event_date) as first_login
     from
         Activity
     group by
         player_id
-),
-
-cte as (
-select
-    f.player_id
-from
-    first_login f
-join
-    Activity a2
-on
-    f.player_id = a2.player_id
-    and f.first_date = a2.event_date - interval 1 day
 )
 
 select
-    round((select count(*) from cte) / count(distinct player_id), 2) as fraction
+    round(count(fl.player_id)/ (select count(distinct player_id) from Activity),2) as fraction
 from
-    Activity
+    first_login fl
+inner join
+    Activity a
+on
+    fl.first_login = a.event_date - interval 1 day
+    and fl.player_id = a.player_id
+
