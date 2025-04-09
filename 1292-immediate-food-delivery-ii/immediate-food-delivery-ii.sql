46cd02e63852d72c83a9e3ae5eeede96c37b780d
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-09 16:54:49 -0700

    Time: 664 ms (80.16%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,19 +1,32 @@
-with cte as (
+with first_order_date as (
     select
         customer_id,
-        min(order_date) as first_order
+        min(order_date) as first_order_date
     from
-        Delivery 
+        Delivery
     group by
         customer_id
+),
+
+order_category as (
+    select
+        f.customer_id,
+        case
+            when d.order_date = d.customer_pref_delivery_date then "immediate"
+            else "scheduled"
+        end as order_category
+    from
+        first_order_date f
+    inner join
+        Delivery d
+    on
+        f.customer_id = d.customer_id
+        and f.first_order_date = d.order_date
 )
 
+-- select * from order_category
+
 select
-    round(100*sum(case when d.order_date = d.customer_pref_delivery_date then 1 else 0 end)* 1.0 / count(*), 2) as immediate_percentage
+    round(100*sum(case when order_category = "immediate" then 1 else 0 end) * 1.0/ count(*),2) as immediate_percentage
 from
-    cte c
-join
-    Delivery d
-on
-    c.customer_id = d.customer_id
-    and c.first_order = d.order_date
+    order_category
