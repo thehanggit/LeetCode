ddda4e95dde30413ee825b5c22188d5c20ae78bb
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-11 08:15:44 -0700

    Time: 1078 ms (45.31%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,48 +1,20 @@
-# Write your MySQL query statement below
--- with project_info as (
---     select
---         P.project_id,
---         E.*
---     from
---         Employee E
---     left join
---         Project P
---     on
---         P.employee_id = E.employee_id
--- )
-
--- select
---     project_id,
---     employee_id
--- from
---     project_info
--- where
---     (project_id, experience_years) in (
---         select
---             project_id,
---             max(experience_years) as most_experience
---         from
---             project_info
---         group by
---             project_id
-        
---     )
+with cte as (
+    select
+        p.project_id,
+        p.employee_id,
+        rank() over(partition by p.project_id order by e.experience_years desc) as ranking
+    from
+        Project p
+    left join
+        Employee e
+    on
+        p.employee_id = e.employee_id
+)
 
 select
-    t.project_id,
-    t.employee_id
+    project_id,
+    employee_id
 from
-    (
-        select
-            project_id,
-            p.employee_id,
-            rank() over(partition by project_id order by experience_years desc) as rnk
-        from
-            Project p
-        join
-            Employee e
-        on
-            p.employee_id = e.employee_id
-    ) t
+    cte
 where
-    t.rnk = 1
+    ranking = 1
