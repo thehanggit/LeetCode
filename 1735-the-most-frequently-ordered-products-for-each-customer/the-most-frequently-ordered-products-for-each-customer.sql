7a35232d1494c5b7f50a9b89302ae1bdc49f23c0
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-11 08:22:53 -0700

    Time: 2264 ms (84.28%) | Memory: 0B (100.00%) - LeetSync

--- 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
+++ 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,8 +1,8 @@
-with ranking as (
+with cte as(
     select
         customer_id,
         product_id,
-        rank() over(partition by customer_id order by count(product_id) desc) as ranking
+        rank() over(partition by customer_id order by count(*) desc) ranking
     from
         Orders
     group by
@@ -10,14 +10,15 @@ with ranking as (
 )
 
 select
-    r.customer_id,
-    p.product_id,
+    c.customer_id,
+    c.product_id,
     p.product_name
 from
-    ranking r
-join
+    cte c
+left join
     Products p
 on
-    r.product_id = p.product_id
+    c.product_id = p.product_id
 where
-    r.ranking = 1
+    ranking = 1
+
