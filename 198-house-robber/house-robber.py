c9dc0765afc95d30278ccb01fea07dcb810fc600
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-29 21:33:44 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (13.32%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @lru_cache(None)
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
+            
+            return max(dp(i - 1), dp(i - 2) + nums[i])
+        return dp(len(nums) - 1)
