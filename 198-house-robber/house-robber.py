4e648011f92ec5a1b427dcb902d011f2217ee218
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-04-29 21:36:46 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (34.43%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,11 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(None)
+        memo = {}
         def dp(i):
-            if i == 0:
-                return nums[0]
-            if i == 1:
-                return max(nums[0], nums[1])
-            
-            return max(dp(i - 1), dp(i - 2) + nums[i])
+            if i < 0:
+                return 0
+            if i in memo:
+                return memo[i]
+            memo[i] = max(dp(i-1), dp(i-2) + nums[i])
+            return memo[i]
         return dp(len(nums) - 1)
