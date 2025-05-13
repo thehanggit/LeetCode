439bd7277e32a774d8337158fb4cba9278e351aa
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-05-12 22:03:23 -0700

    Time: 47 ms (69.40%) | Memory: 18 MB (45.52%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        freq = Counter(digits)
+        ans = []
+        for num in range(100, 1000, 2):
+            parts = [num // 100, num // 10 % 10, num % 10]
+            count  = Counter(parts)
+            if all(freq[d] >= count[d] for d in count):
+                ans.append(num)
+        
+        return ans
