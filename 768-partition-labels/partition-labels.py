f073270a02396d328d43ea0ce3b059451810ae9f
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-07-29 16:47:32 -0700

    Time: 7 ms (39.31%) | Memory: 17.8 MB (34.84%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,37 +1,15 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        # for each letter, we can record their position, or find their "window"
-        # the goal is to find distinct windows such that it covers the whole string
-        
-        # first generate a dict to store those min, max
-        # first = collections.defaultdict(int)
-        # last = collections.defaultdict(int)
-        # for i in range(len(s)):
-        #     if s[i] not in first:
-        #         first[s[i]] = i + 1
-        #         last[s[i]] = i + 1
-        #     else:
-        #         first[s[i]] = min(first[s[i]], i + 1)
-        #         last[s[i]] = max(last[s[i]], i + 1)
-        # ans = []
-        # j = 0
-        # while j != len(s):
-        #     l = first[s[j]]
-        #     r = last[s[j]]
-        #     for key, value in last.items():
-        #         if value > r and first[key] < l:
-        #             r = value
-        #     j = r 
-        #     ans.append([r - l + 1])
-        # return ans
-        last = {ch: i for i, ch in enumerate(s)}  # last index of each character
-        result = []
-        start = end = 0
-        
+        hashmap = {}
         for i, ch in enumerate(s):
-            end = max(end, last[ch])  # extend the partition if needed
-            if i == end:
-                result.append(end - start + 1)
-                start = i + 1  # start of the next partition
+            hashmap[ch] = i
+        size = end = 0
+        ans = []
+        for i, ch in enumerate(s):
+            size += 1
+            end = max(hashmap[ch], end)
+            if end == i:
+                ans.append(size)
+                size = 0
+        return ans
                 
-        return result
