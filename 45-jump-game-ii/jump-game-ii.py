b2a65e1b6e931e2490a5e9ed392ee6f339c50b14
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-07-28 23:53:20 -0700

    Time: 11 ms (42.38%) | Memory: 18.5 MB (75.39%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -2,31 +2,45 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         # greedy method: first, we generate an array for all indices with true that can reach last element
         # then we perform largest steps to true indices from 0 index
-        n = len(nums)
-        feasible_array = [False] * n
-        feasible_array[-1] = True
-        goal = n - 1
-        for i in range(goal, -1, -1):
-            if i + nums[i] >= goal:
-                feasible_array[goal] = True
-                goal = i
+        # n = len(nums)
+        # feasible_array = [False] * n
+        # feasible_array[-1] = True
+        # goal = n - 1
+        # for i in range(goal, -1, -1):
+        #     if i + nums[i] >= goal:
+        #         feasible_array[goal] = True
+        #         goal = i
         
-        # imagine u have found out all feasible positions that can reach last element with T/F, then we can perform dp to solve the minimization problem
-        memo = {}
-        def dp(i):
-            if i >= n - 1:
-                return 0
+        # # imagine u have found out all feasible positions that can reach last element with T/F, then we can perform dp to solve the minimization problem
+        # memo = {}
+        # def dp(i):
+        #     if i >= n - 1:
+        #         return 0
 
-            if i in memo:
-                return memo[i]
+        #     if i in memo:
+        #         return memo[i]
             
-            res = float("inf")
-            for j in range(1, nums[i] + 1):
+        #     res = float("inf")
+        #     for j in range(1, nums[i] + 1):
 
-                if i + j < n and feasible_array[i + j]:
-                    res = min(dp(i + j), res)
+        #         if i + j < n and feasible_array[i + j]:
+        #             res = min(dp(i + j), res)
             
-            memo[i] = res + 1
-            return memo[i]
-        return dp(0)
+        #     memo[i] = res + 1
+        #     return memo[i]
+        # return dp(0)
+
+        # BFS
+        res = 0
+        l = r = 0 # this is the level window
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            
+            l = r + 1
+            r = farthest
+            res += 1
+        
+        return res
         
