a612b8a9e6d516d25216a85118c01354a044afa1
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-07-29 23:05:11 -0700

    Time: 1238 ms (5.07%) | Memory: 0B (100.00%) - LeetSync

--- 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -1,4 +1,6 @@
-with match_points as (
+
+
+with cte as (
     select
         host_team as team,
         case
@@ -11,9 +13,10 @@ with match_points as (
     union all
     select
         guest_team as team,
+
         case
-            when host_goals < guest_goals then 3
-            when host_goals = guest_goals then 1
+            when guest_goals > host_goals then 3
+            when guest_goals = host_goals then 1
             else 0
         end as points
     from
@@ -23,14 +26,16 @@ with match_points as (
 select
     t.team_id,
     t.team_name,
-    ifnull(sum(points), 0) as num_points
+    ifnull(sum(c.points),0) as num_points
 from
     Teams t
 left join
-    match_points mp
+    cte c
 on
-    mp.team = t.team_id
+    t.team_id = c.team
 group by
-    t.team_id, t.team_name
+    t.team_id,
+    t.team_name
 order by
-    num_points desc, t.team_id asc
+    num_points desc,
+    t.team_id asc
