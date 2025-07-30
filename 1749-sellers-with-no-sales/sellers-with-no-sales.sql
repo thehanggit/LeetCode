6ed5e9be0d839dd8fc3cdff04c9fa932911c6cdb
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-07-29 22:22:53 -0700

    Time: 901 ms (58.58%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,20 +1,16 @@
-# Write your MySQL query statement below
+
 select
-    S.seller_name
+    seller_name
 from
-    Seller S
-left join
-    (
+    Seller
+where
+    seller_id not in (
         select
-            distinct seller_id
+            seller_id
         from
             Orders
         where
             year(sale_date) = 2020
-    ) O
-on
-    S.seller_id = O.seller_id
-where
-    O.seller_id is null
+    )
 order by
-    S.seller_name
+    seller_name
