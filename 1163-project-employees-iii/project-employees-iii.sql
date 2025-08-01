9dd49f2791515bb42738e10e77f1062b93586d5f
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-07-31 23:17:50 -0700

    Time: 588 ms (50.99%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,25 +1,3 @@
--- with cte as (
---     select
---         p.project_id,
---         p.employee_id,
---         rank() over(partition by p.project_id order by e.experience_years desc) as ranking
---     from
---         Project p
---     left join
---         Employee e
---     on
---         p.employee_id = e.employee_id
--- )
-
--- select
---     project_id,
---     employee_id
--- from
---     cte
--- where
---     ranking = 1
-
-
 with cte as (
     select
         p.project_id,
