72fdd60a0c9958d16169cf7f61c9278354b0a3b5
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-07-31 23:17:40 -0700

    Time: 630 ms (28.28%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,8 +1,30 @@
+-- with cte as (
+--     select
+--         p.project_id,
+--         p.employee_id,
+--         rank() over(partition by p.project_id order by e.experience_years desc) as ranking
+--     from
+--         Project p
+--     left join
+--         Employee e
+--     on
+--         p.employee_id = e.employee_id
+-- )
+
+-- select
+--     project_id,
+--     employee_id
+-- from
+--     cte
+-- where
+--     ranking = 1
+
+
 with cte as (
     select
         p.project_id,
         p.employee_id,
-        rank() over(partition by p.project_id order by e.experience_years desc) as ranking
+        dense_rank() over(partition by p.project_id order by e.experience_years desc) as rnk
     from
         Project p
     left join
@@ -17,4 +39,4 @@ select
 from
     cte
 where
-    ranking = 1
+    rnk = 1
