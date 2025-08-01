b9c2bbdabf35d4794ace085ef79473513b743003
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-07-31 17:30:27 -0700

    Time: 588 ms (30.75%) | Memory: 0B (100.00%) - LeetSync

--- 1399-page-recommendations/page-recommendations.sql
+++ 1399-page-recommendations/page-recommendations.sql
@@ -1,26 +1,51 @@
-with user_1_friend as (
+-- with user_1_friend as (
+--     select
+--         case
+--             when user1_id = 1 then user2_id
+--             when user2_id = 1 then user1_id
+--         end as id
+--     from
+--         Friendship
+-- )
+
+-- select
+--     distinct(l.page_id) as recommended_page
+-- from
+--     Likes l
+-- inner join
+--     user_1_friend u
+-- on
+--     l.user_id = u.id
+-- where
+--     l.page_id not in (select page_id from Likes where user_id = 1)
+
+
+-- find out friends of user_id = 1
+-- recommend pages of 1's friends liked page_id
+with friends as (
     select
-        user1_id as id
+        case
+            when user1_id = 1 then user2_id
+            when user2_id = 1 then user1_id
+        end as friend
     from
         Friendship
-    where
-        user2_id = 1
-    union
-    select
-        user2_id id
-    from
-        Friendship
-    where
-        user1_id = 1
 )
 
-select
-    distinct(l.page_id) as recommended_page
+select distinct
+    l.page_id as recommended_page
 from
     Likes l
-inner join
-    user_1_friend u
+join
+    friends f
 on
-    l.user_id = u.id
+    l.user_id = f.friend
 where
-    l.page_id not in (select page_id from Likes where user_id = 1)
+    l.page_id not in (
+        select
+            page_id
+        from
+            Likes
+        where
+            user_id = 1
+    )
