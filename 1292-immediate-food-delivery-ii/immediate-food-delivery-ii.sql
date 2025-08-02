ca57567e7c99a96f65c5d88ef48b5c0e3aec28c4
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 15:18:45 -0700

    Time: 573 ms (86.91%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,45 @@
-with first_order_date as (
+-- with first_order_date as (
+--     select
+--         customer_id,
+--         min(order_date) as first_order_date
+--     from
+--         Delivery
+--     group by
+--         customer_id
+-- ),
+
+-- order_category as (
+--     select
+--         f.customer_id,
+--         case
+--             when d.order_date = d.customer_pref_delivery_date then "immediate"
+--             else "scheduled"
+--         end as order_category
+--     from
+--         first_order_date f
+--     inner join
+--         Delivery d
+--     on
+--         f.customer_id = d.customer_id
+--         and f.first_order_date = d.order_date
+-- )
+
+-- -- select * from order_category
+
+-- select
+--     round(100*sum(case when order_category = "immediate" then 1 else 0 end) * 1.0/ count(*),2) as immediate_percentage
+-- from
+--     order_category
+
+
+-- have a column to tell whether this order is immediate or not
+-- find the earliest order for each customoer
+-- inner join these two to get the final answer
+
+with cte as (
     select
         customer_id,
-        min(order_date) as first_order_date
+        min(order_date) as first_order
     from
         Delivery
     group by
@@ -10,23 +48,18 @@ with first_order_date as (
 
 order_category as (
     select
-        f.customer_id,
-        case
-            when d.order_date = d.customer_pref_delivery_date then "immediate"
-            else "scheduled"
-        end as order_category
+        c.customer_id,
+        case when d.order_date = d.customer_pref_delivery_date then 'immediate' else 'scheduled' end as order_type
     from
-        first_order_date f
-    inner join
+        cte c
+    join
         Delivery d
     on
-        f.customer_id = d.customer_id
-        and f.first_order_date = d.order_date
+        c.customer_id = d.customer_id
+        and c.first_order = d.order_date
 )
 
--- select * from order_category
-
 select
-    round(100*sum(case when order_category = "immediate" then 1 else 0 end) * 1.0/ count(*),2) as immediate_percentage
+    round(100*sum(case when order_type = 'immediate' then 1 else 0 end) / count(*),2) as immediate_percentage
 from
     order_category
