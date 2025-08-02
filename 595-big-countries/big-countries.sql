51d9f0e44f4f0a6c742a9e47c1414894e3091ad9
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 11:24:19 -0700

    Time: 266 ms (70.79%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,3 +1,14 @@
+-- select
+--     name,
+--     population,
+--     area
+-- from
+--     World
+-- where
+--     area >= 3000000
+--     or population >= 25000000
+
+
 select
     name,
     population,
