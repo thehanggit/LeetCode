74cdf15c877e37f4f52419007abdbd641db0b384
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 15:43:23 -0700

    Time: 547 ms (52.09%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,27 +1,3 @@
--- select
---     employee_id,
---     department_id
--- from
---     Employee
--- where
---     primary_flag = 'Y'
--- union
--- select
---     employee_id,
---     department_id
--- from
---     Employee
--- where
---     employee_id not in (
---         select
---             employee_id
---         from
---             Employee
---         where
---             primary_flag = 'Y'
---     )
---     and primary_flag = 'N'
-
 select
     employee_id,
     department_id
