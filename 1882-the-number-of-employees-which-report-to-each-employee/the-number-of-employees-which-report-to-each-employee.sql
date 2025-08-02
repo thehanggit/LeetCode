007487b3948690f4504ebac3c73c339237ce371d
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 15:40:14 -0700

    Time: 593 ms (80.12%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,30 +1,3 @@
--- with cte as (
---     select
---         reports_to as employee_id,
---         count(distinct employee_id) as reports_count,
---         round(avg(age)) as average_age
---     from
---         Employees
---     where
---         reports_to is not null
---     group by
---         reports_to
--- )
-
--- select
---     c.employee_id,
---     e.name,
---     c.reports_count,
---     c.average_age
--- from
---     cte c
--- join
---     Employees e
--- on
---     c.employee_id = e.employee_id
--- order by
---     c.employee_id
-
 with cte as (
     select
         reports_to,
