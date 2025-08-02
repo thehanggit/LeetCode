a66f3520dfddfc27e9e4784af8379746db8807fa
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 11:50:58 -0700

    Time: 266 ms (26.49%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,13 +1,29 @@
+-- select
+--     a.machine_id,
+--     round(avg(b.timestamp - a.timestamp) , 3) as processing_time
+-- from
+--     Activity a,
+--     Activity b
+-- where
+--     a.machine_id = b.machine_id
+--     and a.process_id = b.process_id
+--     and a.activity_type = 'start'
+--     and b.activity_type = 'end'
+-- group by
+--     machine_id
+
 select
-    a.machine_id,
-    round(avg(b.timestamp - a.timestamp) , 3) as processing_time
+    a1.machine_id,
+    round(avg(a2.timestamp - a1.timestamp),3) as processing_time
 from
-    Activity a,
-    Activity b
+    Activity a1
+join
+    Activity a2
+on
+    a1.machine_id = a2.machine_id
+    and a1.process_id = a2.process_id
 where
-    a.machine_id = b.machine_id
-    and a.process_id = b.process_id
-    and a.activity_type = 'start'
-    and b.activity_type = 'end'
+    a1.activity_type = 'start'
+    and a2.activity_type = 'end'
 group by
-    machine_id
+    a1.machine_id
