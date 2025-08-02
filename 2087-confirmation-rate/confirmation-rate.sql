c1ce99507e37a7ca8bedee42c9acbfc5ea582354
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 12:12:25 -0700

    Time: 622 ms (74.14%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,11 +1,24 @@
+-- select
+--     s.user_id,
+--     round(sum(case when c.action = 'confirmed' then 1 else 0 end) * 1.0 / count(*), 2) as confirmation_rate
+-- from
+--     Signups s
+-- left join
+--     Confirmations c
+-- on
+--     s.user_id = c.user_id
+-- group by
+--     s.user_id
+
+
 select
     s.user_id,
-    case when c.action is not null then round(sum(case when c.action = 'confirmed' then 1 else 0 end) * 1.0 / sum(case when c.action is not null then 1 else 0 end), 2) else 0 end as confirmation_rate
+    round(sum(case when action = 'confirmed' then 1 else 0 end) * 1.0 / count(*), 2) as confirmation_rate
 from
     Signups s
 left join
     Confirmations c
 on
-    c.user_id = s.user_id
+    s.user_id = c.user_id
 group by
     s.user_id
