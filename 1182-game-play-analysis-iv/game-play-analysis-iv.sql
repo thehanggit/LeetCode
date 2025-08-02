c64edb0c8d4025a57196932585266f33ec646c9c
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 15:23:33 -0700

    Time: 507 ms (91.36%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,3 +1,24 @@
+-- with first_login as (
+--     select
+--         player_id,
+--         min(event_date) as first_login
+--     from
+--         Activity
+--     group by
+--         player_id
+-- )
+
+-- select
+--     round(count(fl.player_id)/ (select count(distinct player_id) from Activity),2) as fraction
+-- from
+--     first_login fl
+-- inner join
+--     Activity a
+-- on
+--     fl.first_login = a.event_date - interval 1 day
+--     and fl.player_id = a.player_id
+
+
 with first_login as (
     select
         player_id,
@@ -9,12 +30,11 @@ with first_login as (
 )
 
 select
-    round(count(fl.player_id)/ (select count(distinct player_id) from Activity),2) as fraction
+    round(count(f.player_id)/ (select count(distinct player_id) from Activity), 2) as fraction
 from
-    first_login fl
-inner join
+    first_login f
+join
     Activity a
 on
-    fl.first_login = a.event_date - interval 1 day
-    and fl.player_id = a.player_id
-
+    f.player_id = a.player_id
+    and datediff(a.event_date, f.first_login) = 1
