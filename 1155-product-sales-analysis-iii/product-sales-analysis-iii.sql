98b88c5ab86712445b84b8a19a19da97a4d750d5
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 21:13:46 -0700

    Time: 640 ms (96.45%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,20 +1,38 @@
-select
+-- select
+--     product_id,
+--     year as first_year,
+--     sum(quantity) as quantity,
+--     price
+-- from
+--     Sales
+-- where
+--     (product_id, year) in (
+--         select
+--             product_id,
+--             min(year) as first_year
+--         from
+--             Sales
+--         group by
+--             product_id
+--     )
+-- group by
+--     product_id, year, price
+ 
+
+ select
     product_id,
     year as first_year,
-    sum(quantity) as quantity,
+    quantity,
     price
-from
+ from
     Sales
 where
     (product_id, year) in (
         select
             product_id,
-            min(year) as first_year
+            min(year)
         from
             Sales
         group by
             product_id
     )
-group by
-    product_id, year, price
- 
