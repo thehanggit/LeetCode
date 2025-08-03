61adcf17953efc741e529010f9e08a85638027bf
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 22:20:29 -0700

    Time: 526 ms (69.41%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,17 +1,34 @@
 # Write your MySQL query statement below
-with condition_met as (
+with cte as (
     select
         tiv_2016,
-        count(*)over(partition by tiv_2015) as tiv_2015_cnt,
-        count(*)over(partition by lat, lon) as loc_cnt
+        count(*) over (partition by tiv_2015) as tiv_2015_cnt,
+        count(*) over (partition by lat, lon) as lat_lon_cnt
     from
-        Insurance
+        insurance
 )
 
 select
     round(sum(tiv_2016),2) as tiv_2016
 from
-    condition_met
+    cte
 where
     tiv_2015_cnt > 1
-    and loc_cnt = 1
+    and lat_lon_cnt = 1
+
+-- with cte as (
+--     select
+--         tiv_2016,
+--         count(*) over(partition by tiv_2015) as tiv_2015_cnt,
+--         count(*) over(partition by (lat, lon)) as lat_lon_cnt
+--     from
+--         Insurance
+-- )
+
+-- select
+--     round(sum(tiv_2016), 2)
+-- from
+--     cte
+-- where
+--     tiv_2015_cnt > 1
+--     and lat_lon_cnt = 1
