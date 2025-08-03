6eee491ff77f92c0ef5c2b817b9e260f768e77c1
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 21:07:00 -0700

    Time: 563 ms (26.65%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,11 +1,3 @@
--- select
---     teacher_id,
---     count(distinct subject_id) as cnt
--- from
---     Teacher
--- group by
---     teacher_id
-
 select
     teacher_id,
     count(distinct(subject_id)) as cnt
