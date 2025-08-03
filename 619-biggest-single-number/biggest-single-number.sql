7c70306101266155d06374d0df9a08db34f3639b
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 21:20:38 -0700

    Time: 363 ms (98.41%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,16 +1,30 @@
-# Write your MySQL query statement below
-with num_count as (
-    select
-        num,
-        count(num) as frequency
-    from
-        MyNumbers
-    group by num
-)
+-- # Write your MySQL query statement below
+-- select
+--     num
+-- from
+--     MyNumbers
+-- group by
+--     num
+-- having
+--     count(*) = 1
+-- union
+-- select null
+-- order by
+--     num desc
+-- limit 1
 
 select
     max(num) as num
 from
-    num_count
-where
-    frequency = 1
+    MyNumbers
+group by
+    num
+having
+    count(*) = 1
+union all
+select
+    null
+order by 
+    num desc
+limit
+    1
