81bd45f0677db327d7dd1678af76cfb352d37b80
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 21:41:41 -0700

    Time: 1395 ms (24.69%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,28 +1,95 @@
-# Write your MySQL query statement below
-(select
-    name as results
-from
-    MovieRating
-join 
-    Users using(user_id)
-group by
-    name
-order by
-    count(*) desc, name
-limit 1)
+-- with cte1 as (
+--     select
+--         u.name,
+--         count(distinct mr.movie_id) as num_movies
+--     from
+--         MovieRating mr
+--     join
+--         Users u
+--     on
+--         mr.user_id = u.user_id
+--     group by
+--         u.name
+--     order by
+--         num_movies desc, u.name
+--     limit
+--         1
+-- ),
 
+-- cte2 as (
+--     select
+--         m.title,
+--         avg(mr.rating) as highest_rating
+--     from
+--         MovieRating mr
+--     join
+--         Movies m
+--     on
+--         mr.movie_id = m.movie_id
+--     where
+--         year(mr.created_at) = 2020 and month(mr.created_at) = 2
+--     group by
+--         m.title
+--     order by
+--         highest_rating desc, title
+--     limit
+--         1
+-- )
+
+-- select
+--     name as results
+-- from
+--     cte1
+-- union all
+-- select
+--     title as results
+-- from
+--     cte2
+
+with cte1 as (
+    select
+        u.name as results
+    from
+        MovieRating m
+    left join
+        Users u
+    on
+        m.user_id = u.user_id
+    group by
+        m.user_id
+    order by
+        count(m.movie_id) desc,
+        u.name
+    limit
+        1
+),
+
+cte2 as (
+    select
+        mv.title
+    from
+        MovieRating m
+    left join
+        Movies mv
+    on
+        m.movie_id = mv.movie_id
+    where
+        date_format(created_at, '%Y-%m') = '2020-02'
+    group by
+        m.movie_id
+    order by
+        avg(m.rating) desc,
+        mv.title
+    limit
+        1
+)
+
+select
+    *
+from
+    cte1
 union all
-
-(select
-    title as results
+select
+    *
 from
-    MovieRating
-join
-    Movies using(movie_id)
-where
-    extract(year_month from created_at) = 202002
-group by
-    title
-order by
-    avg(rating) desc, title
-limit 1)
+    cte2
