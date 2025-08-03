89cde983e57ca503695db02accd89d3ae5b6cd8d
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-02 21:20:45 -0700

    Time: 393 ms (82.33%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,18 +1,3 @@
--- # Write your MySQL query statement below
--- select
---     num
--- from
---     MyNumbers
--- group by
---     num
--- having
---     count(*) = 1
--- union
--- select null
--- order by
---     num desc
--- limit 1
-
 select
     max(num) as num
 from
