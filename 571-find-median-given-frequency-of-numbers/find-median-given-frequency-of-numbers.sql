ed25917f6400cf4508ca4d5aeb0743f12ba3bc1e
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-05 14:04:46 -0700

    Time: 214 ms (64.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 571-find-median-given-frequency-of-numbers/find-median-given-frequency-of-numbers.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+with cte as (
+    select
+        *,
+        sum(frequency) over(order by num) as total_freq,
+        (sum(frequency) over())/2 as median_index
+    from
+        Numbers
+)
+
+select
+    round(avg(num),1) as median
+from
+    cte
+where
+    median_index between (total_freq - frequency) and total_freq
