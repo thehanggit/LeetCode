e45ecc030573692d5a6cf00d29eb083900b5aca5
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-04 23:41:06 -0700

    Time: 3 ms (31.69%) | Memory: 18 MB (57.61%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -2,9 +2,8 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         def backtrack(curr):
-            # base case
-            if len(curr) == len(nums):
-                res.append(curr[:])
+            if len(curr) ==  len(nums):
+                res.append(curr.copy())
                 return
             
             for num in nums:
@@ -12,6 +11,5 @@ class Solution:
                     curr.append(num)
                     backtrack(curr)
                     curr.pop()
-        
         backtrack([])
         return res
