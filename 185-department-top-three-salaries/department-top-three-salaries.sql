6cacae9b1f9ae5198b27d9b60d93d21dd8ba9ff2
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-06 23:53:22 -0700

    Time: 1194 ms (29.72%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,8 +1,31 @@
+-- with ranking as (
+--     select
+--         name,
+--         departmentId,
+--         salary,
+--         dense_rank() over(partition by departmentId order by salary desc) as rnk
+--     from
+--         Employee
+-- )
+
+-- select
+--     d.name as Department,
+--     r.name as Employee,
+--     r.salary as Salary
+-- from
+--     ranking r
+-- join
+--     Department d
+-- on
+--     r.departmentId = d.id
+-- where
+--     rnk <= 3 
+
 with ranking as (
     select
         name,
-        departmentId,
         salary,
+        departmentId,
         dense_rank() over(partition by departmentId order by salary desc) as rnk
     from
         Employee
@@ -19,4 +42,4 @@ join
 on
     r.departmentId = d.id
 where
-    rnk <= 3
+    r.rnk <= 3
