f85d4ed17d0739bf98462443c389cfd51c5f6725
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-08 08:47:12 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (52.32%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,20 +1,38 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # define do(i) as number of decoding ways with suffixs[i:]
+        # define dp(i) as number of decoding ways with suffixs[i:]
+        # memo = {}
+        # n = len(s)
+        # def dp(i):
+        #     if i == n:
+        #         return 1
+        #     if  s[i] == "0":
+        #         return 0
+            
+        #     if i in memo:
+        #         return memo[i]
+            
+        #     memo[i] = dp(i + 1)
+        #     if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
+        #         memo[i] += dp(i + 2)
+                
+        #     return memo[i]
+        # return dp(0)
+
         memo = {}
         n = len(s)
         def dp(i):
             if i == n:
                 return 1
-            if  s[i] == "0":
+            if s[i] == "0":
                 return 0
-            
+                
             if i in memo:
                 return memo[i]
-            
+
             memo[i] = dp(i + 1)
             if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
                 memo[i] += dp(i + 2)
-                
+            
             return memo[i]
         return dp(0)
