b8349e1d26e5e18d2996326dd13d14b1f402d058
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-08 08:32:11 -0700

    Time: 224 ms (38.58%) | Memory: 19.4 MB (41.39%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -27,30 +27,28 @@ class Solution:
         #                 q.append((x_new, y_new, distance + 1))
         # return -1
 
+
         rows = len(grid)
         cols = len(grid[0])
-        dirs = [[-1, 0], [1, 0], [-1, -1], [1, -1], [-1, 1], [1, 1], [0, 1], [0, -1]]
-        q = deque()
-        q.append([0, 0, 1])
-        visited = set()
-        visited.add((0, 0))
-
+        dirs = [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, -1], [1, 0], [1, 1]]
+        queue = deque()
+        seen = set()
+        queue.append((0,0,1))
+        seen.add((0,0))
         if not grid or grid[0][0] or grid[rows-1][cols-1]:
             return -1
-
-        while q:
-            x, y, distance = q.popleft()
-            if (x, y) == (rows-1, cols-1):
-                return distance
+        while queue:
+            r, c, dist = queue.popleft()
+            if (r, c) == (rows-1, cols-1):
+                return dist
 
             for dir in dirs:
-                dx, dy = dir
-                if 0 <= x  + dx <= rows - 1 and 0 <= y + dy <= cols - 1:
-                    row = x + dx
-                    col = y + dy
-                    if grid[row][col] == 0 and (row, col) not in visited:
-                        visited.add((row, col))
-                        q.append((row, col, distance + 1))
-        
-        return -1 
-            
+                new_r = dir[0] + r
+                new_c = dir[1] + c
+                if 0 <= new_r < rows and 0 <= new_c < cols and grid[new_r][new_c] == 0:
+                    if (new_r, new_c) not in seen:
+                        seen.add((new_r, new_c))
+                        queue.append((new_r, new_c, dist+1))
+
+        return -1
+         
