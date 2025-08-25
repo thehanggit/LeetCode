389da7e88a954cf5efa52e834395046911f368ef
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-25 16:36:53 -0700

    Time: 0 ms (100.00%) | Memory: 18.6 MB (20.82%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        def preorder(node, curr_sum):
+            nonlocal count
+            if not node:
+                return
+            
+            curr_sum += node.val
+
+            if curr_sum == k:
+                count += 1
+            
+            count += h[curr_sum - k]
+
+            h[curr_sum] += 1
+            
+            preorder(node.left, curr_sum)
+            preorder(node.right, curr_sum)
+
+            h[curr_sum] -= 1
+
+        count, k = 0, targetSum
+        h = defaultdict(int)
+        preorder(root, 0)
+        return count
+
