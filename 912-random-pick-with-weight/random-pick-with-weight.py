9aaf962f3a61151d064f2a22c3997223680dec8b
Hang Gao <44014340+thehanggit@users.noreply.github.com>
2025-08-27 16:46:22 -0700

    Time: 67 ms (67.50%) | Memory: 23.2 MB (85.68%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -10,11 +10,23 @@ class Solution:
         
 
     def pickIndex(self) -> int:
+
         target = self.total_sum * random.random()
-        for i, prefix_sum in enumerate(self.prefix_sums):
-            if target < prefix_sum:
-                return i
+        # for i, prefix_sum in enumerate(self.prefix_sums):
+        #     if target < prefix_sum:
+        #         return i
         
+        #binary search
+        left = 0
+        right = len(self.prefix_sums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if self.prefix_sums[mid] >= target:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
 
 
 # Your Solution object will be instantiated and called as such:
